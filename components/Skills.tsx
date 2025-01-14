29f9dfe9932024cf6952b53a0769048e36353ca0
pankaj kumar <pankajamsali@gmail.com>
2025-01-14 12:29:13 +0530

    adjust transition duration for Skills component animation from 0.5s to 0.4s for smoother effect

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -29,7 +29,7 @@ export default function Skills() {
     className="mx-auto max-w-2xl md:px-4 px-8 mt-8"
     initial={{ filter: 'blur(10px)' }}
     animate={{ filter: 'blur(0px)' }}
-    transition={{ duration: 0.5 }}
+    transition={{ duration: 0.4 }}
   >
       <h2 className="text-xl text-slate-100">Skills & Tools</h2>
       <div className="flex flex-wrap gap-2 mt-2 max-w-2xl">
