d3d40006d934631b89f89c7cc5a17a7f9ad7d3b0
pankaj kumar <pankajamsali@gmail.com>
2025-01-14 12:19:30 +0530

    add framer-motion for animations; implement hover effects on About, Skills, Projects, and Contact sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import AboutSection from '@/components/about-section';
 import Contact from '@/components/Contact';
 import Projects from '@/components/Projects';
@@ -5,11 +6,35 @@ import Skills from '@/components/Skills';
 
 export default function Home() {
   return (
-    <div className="flex flex-col   w-full text-slate-300 mx-auto ">
-      <AboutSection />  
-      <Skills />
-      <Projects />
-      <Contact/>
+    <div className="flex flex-col w-full text-slate-300 mx-auto">
+      <motion.div
+        initial={{ filter: 'blur(0px)' }}
+        whileHover={{ filter: 'blur(5px)' }}
+        transition={{ duration: 0.5 }}
+      >
+        <AboutSection />
+      </motion.div>
+      <motion.div
+        initial={{ filter: 'blur(0px)' }}
+        whileHover={{ filter: 'blur(5px)' }}
+        transition={{ duration: 0.5 }}
+      >
+        <Skills />
+      </motion.div>
+      <motion.div
+        initial={{ filter: 'blur(0px)' }}
+        whileHover={{ filter: 'blur(5px)' }}
+        transition={{ duration: 0.5 }}
+      >
+        <Projects />
+      </motion.div>
+      <motion.div
+        initial={{ filter: 'blur(0px)' }}
+        whileHover={{ filter: 'blur(5px)' }}
+        transition={{ duration: 0.5 }}
+      >
+        <Contact />
+      </motion.div>
     </div>
   );
 }
